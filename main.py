d5ae9bce008d9b8b323ca917b799d2bd74898adf
Agnar Martin Bjørnstad <agnar.bjornstad@gmail.com>
2025-03-06 19:34:42 +0100

    docs: provide example code for the library

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
-from gradflow.optim.sgd import SGD
-from gradflow.nn.modules.loss import MSELoss
 import gradflow as gf
 
 
-if __name__ == "__main__":
-    t = gf.tensor([1, 2, 3], dtype=float)
-    optim = SGD([t])
+w = gf.tensor([1, 2, 3], dtype=float)
+x = gf.tensor([2, 0.5, 1], dtype=float, requires_grad=False)
+lr = 0.5
 
-    i = gf.tensor([[1, 4], [1, 2]], dtype=float).reshape(2, 2)
-    optim = SGD([i])
-
-    for idx in range(1000):
-        t = gf.tensor([1, 1], dtype=float).reshape(2, 1)
-        s = i.T @ t
-        j = i[1, 0]
-        optim.zero_grad()
-        loss = MSELoss()(s, gf.tensor([-2, 4], dtype=float).reshape(2, 1)) + j**2
-        loss.backward()
-        optim.step()
-        print(loss, i)
+for i in range(10):
+    loss = (w.softmax(dim=0) * x).sum()
+    loss.backward()
+    w = w - lr * w.grad
+    print(f"Loss: {loss}, w: {w}")
