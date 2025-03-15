179cdb431c9d96da59989cb6872806b32803b265
Agnar Martin Bjørnstad <agnar.bjornstad@gmail.com>
2025-03-15 21:20:18 +0100

    refactor: move train/val loop to dedicated function

--- mnist.py
+++ mnist.py
@@ -1,13 +1,15 @@
 from gradflow.nn.modules import linear, module, container, activation
 from gradflow.nn.modules.loss import MSELoss, CrossEntropyLoss
-from gradflow.autograd.grad_mode import no_grad
+from gradflow.autograd.grad_mode import no_grad, set_grad_enabled
 from gradflow.nn.functional import relu, softmax
 from gradflow.optim.adam import Adam
+from gradflow.optim.optimizer import Optimizer
 import gzip
 import matplotlib.pyplot as plt
 import numpy as np
 import gradflow as gf
 import cv2
+from typing import Optional
 from tqdm import tqdm
 
 
@@ -39,7 +41,68 @@ def read_images(path):
     return gf.tensor(images)
 
 
+def run_epoch(
+    model,
+    x,
+    y,
+    loss_fn,
+    batch_size,
+    epoch,
+    epochs,
+    optimizer: Optional[Optimizer] = None,
+    train: bool = True,
+):
+    dataset_str = "Train" if train else "Val"
+    correct = 0
+    total = 0
+    curr_loss = 0
+
+    bar = tqdm(
+        range(0, len(x), batch_size), desc="{dataset_str} epoch {epoch}/{epochs}"
+    )
+    for i in bar:
+        with set_grad_enabled(train):
+            end = min(i + batch_size, len(x))
+            p = model(x[i:end].reshape(end - i, -1) / 255)
+            target = gf.zeros_like(p)
+            target[np.array(range(end - i)), y[i:end, 0]] = 1
+            loss = loss_fn(p, target)
+
+            curr_loss = loss.detach().numpy()[0]
+            curr_loss += curr_loss * (end - i)
+            total += end - i
+
+            correct += (
+                np.array(p.argmax(axis=-1) - np.array(y[i:end]).reshape(-1)) == 0
+            ).sum()
+
+            bar.set_description(
+                f"{dataset_str} epoch {epoch}/{epochs}: loss {(curr_loss/total):.5f}, acc. {correct/total*100:.2f}%"
+            )
+
+            if train:
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+    return curr_loss, correct, total
+
+
 if __name__ == "__main__":
+    BATCH_SIZE = 128
+    EPOCHS = 10
+    LR = 0.001
+
+    model = container.Sequential(
+        linear.Linear(784, 128),
+        activation.ReLU(),
+        linear.Linear(128, 128),
+        activation.ReLU(),
+        linear.Linear(128, 10),
+        activation.Softmax(),
+    )
+    loss_fn = CrossEntropyLoss()
+    optimizer = Adam(model.parameters(), lr=LR)
+
     mnist_path = "data/mnist"
     train_labels_path = f"{mnist_path}/train-labels-idx1-ubyte.gz"
     train_x_path = f"{mnist_path}/train-images-idx3-ubyte.gz"
@@ -51,80 +114,31 @@ if __name__ == "__main__":
     val_x = read_images(val_x_path)
     val_y = read_labels(val_labels_path)
 
-    model = container.Sequential(
-        linear.Linear(784, 128),
-        activation.ReLU(),
-        linear.Linear(128, 128),
-        activation.ReLU(),
-        linear.Linear(128, 10),
-        activation.Softmax(),
-    )
-    loss_fn = CrossEntropyLoss()
-    lr = 0.001
-    optimizer = Adam(model.parameters(), lr=lr)
-
-    losses = []
-    val_losses = []
-    accuracy = []
-    val_accuracy = []
-    batch_size = 128
-    epochs = 1000
-
-    for epoch in range(epochs):
-        correct = 0
-        total = 0
-        val_correct = 0
-        val_total = 0
-        curr_train_loss = 0
-        curr_val_loss = 0
-        train_bar = tqdm(
-            range(0, len(train_x), batch_size), desc="Train epoch {epoch}/{epochs}"
+    for epoch in range(EPOCHS):
+        train_loss, train_correct, num_train_samples = run_epoch(
+            model,
+            train_x,
+            train_y,
+            loss_fn,
+            BATCH_SIZE,
+            epoch,
+            EPOCHS,
+            optimizer,
+            train=True,
         )
-        for i in train_bar:
-            optimizer.zero_grad()
-            end = min(i + batch_size, len(train_x))
-            p = model(train_x[i:end].reshape(end - i, -1) / 255)
-            target = gf.zeros_like(p)
-            target[np.array(range(end - i)), train_y[i:end, 0]] = 1
-            loss = loss_fn(p, target)
-            loss.backward()
+        val_loss, val_correct, num_val_samples = run_epoch(
+            model,
+            train_x,
+            train_y,
+            loss_fn,
+            BATCH_SIZE,
+            epoch,
+            EPOCHS,
+            optimizer,
+            train=False,
+        )
 
-            curr_loss = loss.detach().numpy()[0]
-            losses.append(curr_loss)
-            curr_train_loss += curr_loss * (end - i)
-            total += end - i
-
-            correct += (
-                np.array(p.argmax(axis=-1) - np.array(train_y[i:end]).reshape(-1)) == 0
-            ).sum()
-            train_bar.set_description(
-                f"Train epoch {epoch}/{epochs}: loss {(curr_train_loss/total):.5f}, acc. {correct/total*100:.2f}%"
-            )
-
-            optimizer.step()
-
-        with no_grad():
-            val_bar = tqdm(
-                range(0, len(val_x), batch_size), desc="Val epoch {epoch}/{epochs}"
-            )
-            for i in val_bar:
-                end = min(i + batch_size, len(val_x))
-                p = model(val_x[i:end].reshape(end - i, -1) / 255)
-                target = gf.zeros_like(p)
-                target[np.array(range(end - i)), val_y[i:end, 0]] = 1
-                loss = loss_fn(p, target)
-
-                curr_loss = loss.detach().numpy()[0]
-                val_losses.append(curr_loss)
-                val_correct += (
-                    np.array(p.argmax(axis=-1) - np.array(val_y[i:end]).reshape(-1))
-                    == 0
-                ).sum()
-                val_total += end - i
-                curr_val_loss += curr_loss * (end - i)
-
-                val_bar.set_description(
-                    f"Val epoch {epoch}/{epochs}: loss {(curr_val_loss/val_total):.5f} acc. {val_correct/val_total*100:.2f}%"
-                )
-        loss = curr_train_loss / total
-        val_loss = curr_val_loss / val_total
+    print(f"Train loss: {train_loss/num_train_samples}")
+    print(f"Train accuracy: {train_correct/num_train_samples*100}%")
+    print(f"Val loss: {val_loss/num_val_samples}")
+    print(f"Val accuracy: {val_correct/num_val_samples*100}%")
