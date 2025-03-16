c0b6e8272da4867267b6636e5cfa9983b6f6513f
Agnar Martin Bjørnstad <agnar.bjornstad@gmail.com>
2025-03-16 13:10:44 +0100

    fix: reorganize imports

--- gradflow/autograd/__init__.py
+++ gradflow/autograd/__init__.py
@@ -5,4 +5,4 @@ from .grad_mode import (  # noqa: F401
     set_grad_enabled,
 )
 
-__all__ = [is_grad_enabled, no_grad, enable_grad, set_grad_enabled]
+__all__ = ["is_grad_enabled", "no_grad", "enable_grad", "set_grad_enabled"]
